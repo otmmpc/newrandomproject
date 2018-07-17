12d551e50b140d967540971d755fd21fed00b25b
otmmpc <40898453+otmmpc@users.noreply.github.com>
2018-07-17 18:14:44 -0400

    upload  files
    upload files

---
+++ data.js
@@ -0,0 +1,144 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-seatac_equi_1_72",
+      "name": "SeaTAc_Equi_1_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": -0.6758975427110343,
+        "pitch": 0.054817267122338365,
+        "fov": 1.4325132231102349
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.103779559164918,
+          "pitch": 0.08675628690409809,
+          "rotation": 0,
+          "target": "1-seatac_equi_2_72"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5523280629525988,
+          "pitch": 0.0553982130806574,
+          "title": "Coffee",
+          "text": "Some Text Goes here...&nbsp;&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-seatac_equi_2_72",
+      "name": "SeaTAc_Equi_2_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": 3.0975870519902617,
+        "pitch": 0.11664354614533323,
+        "fov": 1.4325132231102349
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0975870519902617,
+          "pitch": 0.11664354614533323,
+          "rotation": 0,
+          "target": "0-seatac_equi_1_72"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-seatac_equi_3_72",
+      "name": "SeaTAc_Equi_3_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": 2.246118718159524,
+        "pitch": 0.1059767435464245,
+        "fov": 1.4325132231102349
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1015050950106247,
+          "pitch": -0.006721102845357763,
+          "rotation": 0,
+          "target": "1-seatac_equi_2_72"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
